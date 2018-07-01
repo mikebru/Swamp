8a5121f96bdd1ecfde97acaacffd1547e8c0b6ee
Michael Bruner <michael.bruner3@gmail.com>
2018-07-01 15:57:47 -0500

    Initial Commit

---
+++ Assets/Scripts/FlyCam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyCam : MonoBehaviour {
+
+	private Rigidbody camRigidbody;
+
+	[Range(.1f, 10)]
+	public float Speed = 3;
+
+	// Use this for initialization
+	void Start () {
+		camRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float xInput = Input.GetAxis ("Horizontal");
+		float yInput = Input.GetAxis ("Vertical");
+
+		Vector3 currentInput = new Vector3(xInput, 0, yInput);
+
+		camRigidbody.AddForce (currentInput);
+
+
+	}
+}
