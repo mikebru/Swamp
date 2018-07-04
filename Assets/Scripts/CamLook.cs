aa3cfba2c0d5ea06968ba779c9532305121eef5b
Michael Bruner <michael.bruner3@gmail.com>
2018-07-04 12:26:51 -0500

    updated character controller

---
+++ Assets/Scripts/CamLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLook : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 mousePoint = Input.mousePosition;
+
+		float xRotate = Mathf.Lerp (-1, 1, (mousePoint.x * 1.0f / Screen.width* 1.0f ));
+		float yRotate = Mathf.Lerp (1, -1, (mousePoint.y * 1.0f / Screen.height* 1.0f ));
+
+		transform.Rotate (new Vector3(yRotate,xRotate, 0));
+
+		transform.rotation = Quaternion.Euler(new Vector3 (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0));
+	}
+}
--- Assets/Scripts/RaftMovement.cs
+++ Assets/Scripts/RaftMovement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class RaftMovement : MonoBehaviour {
 
+	public Camera rigCam;
 	private Rigidbody camRigidbody;
+
+	public GameObject WaterRipple;
 
 	[Range(10, 1000)]
 	public float Force = 3;
@@ -14,34 +17,76 @@ public class RaftMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		camRigidbody = GetComponent<Rigidbody> ();
+
+		rigCam = GetComponentInChildren<Camera> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+		Paddle ();
+
+		CameraRotation ();
+	}
+
+
+	void Paddle()
+	{
 		float xInput = Input.GetAxis ("Horizontal");
 		float yInput = Input.GetAxis ("Vertical");
 
-		if (PaddleIn == false && (xInput !=0 || yInput != 0)) {
-			
-			Vector3 currentInput = new Vector3 (xInput, 0, yInput) * Force;
+		if ((xInput != 0 || yInput != 0)) {
+
+			Vector3 currentInput = ((transform.forward * yInput) + (transform.right * xInput)) * Force;
 
 			Vector3 currentVelocity = camRigidbody.velocity;
 
 			Vector3 heading = currentInput.normalized - currentVelocity.normalized;
 
-			camRigidbody.AddForce (currentInput + (Vector3.Scale(currentVelocity, heading)));
+			camRigidbody.AddForce (currentInput + (Vector3.Scale (currentVelocity, heading)));
 
-			PaddleIn = true;
-			Debug.Log ("Paddle");
-			Debug.Log (heading);
+			if (PaddleIn == false) {
+				//StartCoroutine (RippleGenerate ());
 
-		} 
-
-		if (xInput == 0 && yInput == 0) {
+				PaddleIn = true;
+			}
+		} else if(PaddleIn == true){
 			PaddleIn = false;
 		}
+	}
 
+	void CameraRotation()
+	{
+		Vector2 mousePoint = Input.mousePosition;
+
+		float xRotate = Mathf.Lerp (-1, 1, (mousePoint.x * 1.0f / Screen.width* 1.0f ));
+		float yRotate = Mathf.Lerp (1, -1, (mousePoint.y * 1.0f / Screen.height* 1.0f ));
+
+		//Rotate Raft to set forward direction
+		transform.Rotate (new Vector3(0,xRotate, 0));
+
+		//lock raft values
+		transform.rotation = Quaternion.Euler(new Vector3 (0, transform.rotation.eulerAngles.y, 0));
+
+		//Rotate cam up and own for viewing
+		rigCam.transform.Rotate(new Vector3(yRotate,0, 0));
+
+		//lock cam values
+		rigCam.transform.localRotation = Quaternion.Euler(new Vector3 (rigCam.transform.localRotation.eulerAngles.x, 0, 0));
 
 	}
+
+
+	IEnumerator RippleGenerate()
+	{
+		Instantiate (WaterRipple, new Vector3(transform.position.x, 0, transform.position.z), Quaternion.identity);
+
+		yield return new WaitForSeconds (.5f);
+
+		if (PaddleIn == true) {
+			StartCoroutine (RippleGenerate ());
+		}
+	}
+
+
 }
